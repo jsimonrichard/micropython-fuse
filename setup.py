f10f7b773023404b653704724891e4788e8231e6
jsimonrichard <jsimonrichard@gmail.com>
2022-01-29 16:52:18 -0500

    Create python environment

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="micropython-fuse",
+    version="0.0.1",
+    author="Simon Richard",
+    author_email="jsimonrichard@gmail.com",
+    description="A FUSE filesystem for micropython boards that communicates over the REPL",
+    scripts=""
+)
